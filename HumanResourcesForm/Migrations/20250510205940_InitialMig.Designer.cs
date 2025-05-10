cb92de63bf735c5f539008b374f726adabfdb1e1
soner keserci <sonerkeserci@gmail.com>
2025-05-11 00:00:16 +0300

    Fixed migration

--- HumanResourcesForm/Migrations/20250510205940_InitialMig.Designer.cs
+++ HumanResourcesForm/Migrations/20250510205940_InitialMig.Designer.cs
@@ -3,6 +3,7 @@ using HumanResourcesForm;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HumanResourcesForm.Migrations
 {
     [DbContext(typeof(HRContext))]
-    partial class HRContextModelSnapshot : ModelSnapshot
+    [Migration("20250510205940_InitialMig")]
+    partial class InitialMig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
