507eb501e4f41fdadf555547fdb3acee2f6fd1fe
soner keserci <sonerkeserci@gmail.com>
2025-05-11 17:21:52 +0300

    Adding applicants controller.

---
+++ HumanResourcesForm/Controllers/ApplicantsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HumanResourcesForm.Data;
+
+namespace HumanResourcesForm.Controllers
+{
+    public class ApplicantsController : Controller
+    {
+        private readonly HRContext _context;
+        public ApplicantsController(HRContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var applicants = _context.Applicants.ToList();
+            return View(applicants);
+        }
+    }
+}
