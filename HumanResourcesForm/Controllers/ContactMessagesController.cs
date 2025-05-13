b9176f7b30c059b082ae0c25c5e9c2cca64ec8fb
soner keserci <sonerkeserci@gmail.com>
2025-05-13 04:13:22 +0300

    Adding ContactMessages controller and views

---
+++ HumanResourcesForm/Controllers/ContactMessagesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HumanResourcesForm.Data;
+using HumanResourcesForm.Models;
+
+namespace HumanResourcesForm.Controllers
+{
+    public class ContactMessagesController : Controller
+    {
+        private readonly HRContext _context;
+
+        public ContactMessagesController(HRContext context)
+        {
+            _context = context;
+        }
+
+        // GET 
+        public async Task<IActionResult> Index()
+        {
+            var hRContext = _context.ContactMessages.Include(c => c.Applicant);
+            return View(await hRContext.ToListAsync());
+        }
+
+        // GET: Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contactMessage = await _context.ContactMessages
+                .Include(c => c.Applicant)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contactMessage == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactMessage);
+        }
+
+        // GET: Create
+        public IActionResult Create()
+        {
+            ViewData["ApplicantId"] = new SelectList(_context.Applicants, "Id", "Email");
+            return View();
+        }
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Message,SentAt,ApplicantId")] ContactMessage contactMessage)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contactMessage);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApplicantId"] = new SelectList(_context.Applicants, "Id", "Email", contactMessage.ApplicantId);
+            return View(contactMessage);
+        }
+
+        // GET: Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contactMessage = await _context.ContactMessages.FindAsync(id);
+            if (contactMessage == null)
+            {
+                return NotFound();
+            }
+            ViewData["ApplicantId"] = new SelectList(_context.Applicants, "Id", "Email", contactMessage.ApplicantId);
+            return View(contactMessage);
+        }
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Message,SentAt,ApplicantId")] ContactMessage contactMessage)
+        {
+            if (id != contactMessage.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contactMessage);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContactMessageExists(contactMessage.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApplicantId"] = new SelectList(_context.Applicants, "Id", "Email", contactMessage.ApplicantId);
+            return View(contactMessage);
+        }
+
+        // GET: Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contactMessage = await _context.ContactMessages
+                .Include(c => c.Applicant)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contactMessage == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactMessage);
+        }
+        // POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contactMessage = await _context.ContactMessages.FindAsync(id);
+            if (contactMessage != null)
+            {
+                _context.ContactMessages.Remove(contactMessage);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContactMessageExists(int id)
+        {
+            return _context.ContactMessages.Any(e => e.Id == id);
+        }
+    }
+}
